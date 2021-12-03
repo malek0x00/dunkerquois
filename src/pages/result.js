94f0449a5748c8d0fedac703e402dee62c456b9a
Malek Bougadouha <bougadouha.malek@gmail.com>
2021-12-03 01:19:57 +0100

    edseds

--- src/pages/result.js
+++ src/pages/result.js
@@ -1,10 +1,14 @@
+import  NavBar  from '../components/bar';
 import React from 'react';
 
 function ResultPage() {
     return ( 
         <div>
+            <NavBar/>
+            <div style={{marginTop:90}}>
+                aaa
+            </div>
 
-            
         </div>
 
      );
